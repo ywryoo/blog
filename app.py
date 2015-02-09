a1b4b74a6919cbd9276f0cedd421112a366b52a0
ywryoo <ywryoo@gmail.com>
2015-02-09 17:49:45 +0900

    Ubuntu server tesing

--- app.py
+++ app.py
@@ -1,47 +1,16 @@
-import os
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, jsonify
 
-
-#initialization
 app = Flask(__name__)
-app.config.update(
-    DEBUG=True,
-)
 
-
-#controllers
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'ico/favicon.ico')
-
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
-
-
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template('index.html')
+    return 'Flask is running!'
+
+@app.route('/data')
+def names():
+    data = {"names":["John","Jacob","Julie","Jennifer"]}
+    return jsonify(data)
 
 
-@app.route("/list")
-def list():
-    return render_template('list.html')
-
-
-@app.route("/search")
-def search():
-    return render_template('search.html')
-
-
-@app.route("/post")
-def post():
-    return render_template('post.html')
-
-
-#launch
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+if __name__=='__main__':
+    app.run()
