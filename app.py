6c5d245f55596482f175eb0839904fac1270bbbe
ywryoo <ywryoo@gmail.com>
2015-02-10 17:14:28 +0900

    corrected by linter

--- app.py
+++ app.py
@@ -2,17 +2,18 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return 'Flask is running!'
 
+
 @app.route('/data')
 def names():
-    data = {"first_names":["John","Jacob","Julie","Jennifer"],
-	    "last_names":["Connor","Johnson","Cloud","Ray"]
-	   }
+    data = {"first_names": ["John", "Jacob", "Julie", "Jennifer"],
+            "last_names": ["Connor", "Johnson", "Cloud", "Ray"]}
     return jsonify(data)
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run()
