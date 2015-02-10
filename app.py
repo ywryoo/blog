274afea208aea064f9f21c49d99e0a31606a78bf
ywryoo <ywryoo@gmail.com>
2015-02-10 17:42:27 +0900

    Add 404 error handler

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 
@@ -13,6 +13,11 @@ def names():
     data = {"first_names": ["John", "Jacob", "Julie", "Jennifer"],
             "last_names": ["Connor", "Johnson", "Cloud", "Ray"]}
     return jsonify(data)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 
 if __name__ == '__main__':
